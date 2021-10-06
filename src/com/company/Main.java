3e12069cf55fb6ce534a02a1602907c36e2803c4
KERRCAM <kerrcameron@hotmail.com>
2021-10-06 16:46:18 +0100

    user turns done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,13 +19,31 @@ public class Main{
         cards[i] = JacksQueensKings;
     }
 
-
     Random random  = new Random();
     Scanner input = new Scanner(System.in);
-    int CardDrawn = random.nextInt(52);
-    System.out.println(CardDrawn);
-    System.out.println(cards[CardDrawn]);
+    int UserDraws = 2;
+    int CardDrawnOne = random.nextInt(52);
+    int CardDrawnTwo = random.nextInt(52);
+    int UserTotal = (CardDrawnOne + CardDrawnTwo);
+    System.out.println("you drew a " + CardDrawnOne + " and a " + CardDrawnTwo + ". Your total is: " + UserTotal);
 
+    boolean UserTurn = false;
+    while(UserTurn == false) {
+        System.out.println("would you like to drawn again? (yes or no): ");
+        String DrawAgain = input.nextLine();
+        if (DrawAgain == "yes") {
+            int CardDrawnExtra = random.nextInt(52);
+            UserTotal = UserTotal + CardDrawnExtra;
+            System.out.println("your new total is " + UserTotal);
+            UserDraws = UserDraws+1;
+        }
+        if (DrawAgain == "no") {
+            System.out.println("your final total is " + UserTotal);
+            UserTurn = true;
+        }
+
+
+    }
 
 
     }
