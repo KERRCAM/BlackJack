530dee7293628f84f728ab53e8c847e0356626ac
KERRCAM <kerrcameron@hotmail.com>
2021-10-06 16:27:06 +0100

    list loops done.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Random;
 import java.util.Scanner;
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
     int cards[] = new int[53];
